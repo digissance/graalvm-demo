05f00fccfb284e3689e79aa970f8b9edce619506
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-02-09 10:24:22 +0100

    fix: inital commit

---
+++ src/test/java/biz/digissance/graalvmdemo/GraalvmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.digissance.graalvmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
