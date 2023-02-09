1600040fb4706e2c92056ef4d80543639574e25b
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-02-09 20:42:40 +0100

    fix: adding another controller with post method

--- src/main/java/biz/digissance/graalvmdemo/GraalvmDemoApplication.java
+++ src/main/java/biz/digissance/graalvmdemo/GraalvmDemoApplication.java
@@ -2,8 +2,11 @@ package biz.digissance.graalvmdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +25,10 @@ public class GraalvmDemoApplication {
     @GetMapping("/hello/{var}")
     public String another(@PathVariable final String var) {
         return "hello " + var;
+    }
+
+    @PostMapping("/pojo")
+    public ResponseEntity<SimplePojo> pojo(@RequestBody final SimplePojo pojo) {
+        return ResponseEntity.ok(pojo);
     }
 }
