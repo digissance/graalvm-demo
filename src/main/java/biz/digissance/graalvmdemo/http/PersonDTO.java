5d6e57a5aea765e305daf581bafee12fd4771b6d
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-02-12 23:53:11 +0100

    fix: adding jpa test

--- src/main/java/biz/digissance/graalvmdemo/http/PersonDTO.java
+++ src/main/java/biz/digissance/graalvmdemo/http/PersonDTO.java
@@ -2,20 +2,27 @@ package biz.digissance.graalvmdemo.http;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
-@Value
-@Builder
+@Data
+//@Value
+//@Builder
 @Jacksonized
-public class PersonDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO{
 
     @NotBlank
-    String name;
+    private String name;
     @NotBlank
-    String lastname;
+    private String lastname;
     @Email
     @NotBlank
-    String email;
+    private String email;
 }
