ad11adc606ddaf86b22ef8741e363d4468e8cdbe
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-03-29 09:29:13 +0200

    fix: adjusting to deploy in fly.io

--- src/main/java/biz/digissance/graalvmdemo/http/admin/party/PartyRoleTypeController.java
+++ src/main/java/biz/digissance/graalvmdemo/http/admin/party/PartyRoleTypeController.java
@@ -1,6 +1,8 @@
 package biz.digissance.graalvmdemo.http.admin.party;
 
+import biz.digissance.graalvmdemo.jpa.party.role.JpaPartyRoleTypeRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class PartyRoleTypeController {
 
+    private final JpaPartyRoleTypeRepository repository;
+
+    public PartyRoleTypeController(final JpaPartyRoleTypeRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/party-role-types")
-    public String partyRoleTypesView() {
+    public String partyRoleTypesView(Model model) {
+        model.addAttribute("roleTypes", repository.findAll());
         return "party-role-types";
     }
 }
