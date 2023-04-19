0c39360d7bf986b48998fabe630c450a2df45266
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-04-19 18:14:33 +0200

    fix: adjusting sso

--- src/main/java/biz/digissance/graalvmdemo/security/MySecurityContextRepository.java
+++ src/main/java/biz/digissance/graalvmdemo/security/MySecurityContextRepository.java
@@ -1,9 +1,12 @@
 package biz.digissance.graalvmdemo.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
@@ -21,12 +24,10 @@ import org.springframework.security.web.context.HttpRequestResponseHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
 
 class MySecurityContextRepository implements SecurityContextRepository {
-    private final ObjectMapper mapper;
     private final String SECURITY_CONTEXT_COOKIE_NAME;
 
-    public MySecurityContextRepository(final ObjectMapper objectMapper, final String SECURITY_CONTEXT_COOKIE_NAME) {
+    public MySecurityContextRepository(final String SECURITY_CONTEXT_COOKIE_NAME) {
         this.SECURITY_CONTEXT_COOKIE_NAME = SECURITY_CONTEXT_COOKIE_NAME;
-        this.mapper = objectMapper;
     }
 
     @Override
@@ -42,28 +43,39 @@ class MySecurityContextRepository implements SecurityContextRepository {
 
     @SneakyThrows
     private SecurityContext deSerializeCookie(final Cookie cookie) {
-        final var p = mapper.readValue(Base64.getDecoder().decode(cookie.getValue()), Map.class);
-        final List<GrantedAuthority> roles =
-                ((List<String>) p.getOrDefault("roles", new ArrayList<>())).stream().map(
-                        SimpleGrantedAuthority::new).collect(Collectors.toList());
-        final var auth =
-                new PreAuthenticatedAuthenticationToken(p.get("sub"), null, roles);
-        return new SecurityContextImpl(auth);
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(Base64.getDecoder().decode(cookie.getValue().getBytes()));
+        try (ObjectInputStream ois = new ObjectInputStream(bis)) {
+            final Map<String, Object> p = (Map<String, Object>) ois.readObject();
+            final List<GrantedAuthority> roles =
+                    ((List<String>) p.getOrDefault("roles", new ArrayList<>())).stream().map(
+                            SimpleGrantedAuthority::new).collect(Collectors.toList());
+            final var auth =
+                    new PreAuthenticatedAuthenticationToken(p.get("sub"), null, roles);
+            return new SecurityContextImpl(auth);
+        }
     }
 
     @Override
     @SneakyThrows
     public void saveContext(final SecurityContext context, final HttpServletRequest request,
                             final HttpServletResponse response) {
-        final String secContext;
-        final var roles = context.getAuthentication()
-                .getAuthorities().stream().map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-        final var sec = Map.of("sub", context.getAuthentication().getName(), "roles", roles);
-        secContext = Base64.getEncoder().encodeToString(mapper.writeValueAsBytes(sec));
-        Cookie cookie = new Cookie(SECURITY_CONTEXT_COOKIE_NAME, secContext);
-        cookie.setPath("/");
-        response.addCookie(cookie);
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            final String secContext;
+            final var roles = context.getAuthentication()
+                    .getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+            final var sec = Map.of("sub", context.getAuthentication().getName(), "roles", roles);
+            ObjectOutputStream out = null;
+            out = new ObjectOutputStream(bos);
+            out.writeObject(sec);
+            out.flush();
+            byte[] yourBytes = bos.toByteArray();
+            secContext = Base64.getEncoder().encodeToString(yourBytes);
+            Cookie cookie = new Cookie(SECURITY_CONTEXT_COOKIE_NAME, secContext);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        }
     }
 
     @Override
